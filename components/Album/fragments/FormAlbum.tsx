6f3a16cd2ed85652c53e6c3877de9b80b2defbe6
Jacques <jacques@Mac-mini-de-Raph.local>
2023-03-07 09:07:21 +0100

    WIP form update

--- components/Album/fragments/FormAlbum.tsx
+++ components/Album/fragments/FormAlbum.tsx
@@ -12,24 +12,24 @@ type Props = {
 
 const FormAlbum = ({formLabel, visibility, changeVisible, actionAlbum, isEdit, album}: Props) => {
 
-    const[albumName, setAlbumName] = useState<string>(isEdit ? album?.name : "");
+    const[albumName, setAlbumName] = useState<string>("");
     const[groupe, setGroupe] = useState<string>("");
     const[nbTitle,setNbTitle] = useState<number>(0);
     const[styleOne, setStyleOne] = useState<string>("");
     const[image, setImage] = useState<string>("");
     const[releaseDate, setReleaseDate] = useState<string>("");
 
-    useEffect(() => {
-        if(album){
-            console.log(isEdit ? album?.name : "");
-            // setAlbumName(album.name);
-            setGroupe(album.groupe);
-            setNbTitle(album.nb_title);
-            setStyleOne(album.styleOne);
-            setImage(album.image);
-            setReleaseDate(album.release_date);
-        }
-    },[visibility])
+    // useEffect(() => {
+    //     if(album){
+    //         console.log(isEdit ? album?.name : "");
+    //         // setAlbumName(album.name);
+    //         setGroupe(album.groupe);
+    //         setNbTitle(album.nb_title);
+    //         setStyleOne(album.styleOne);
+    //         setImage(album.image);
+    //         setReleaseDate(album.release_date);
+    //     }
+    // },[visibility])
 
     return (
         <div className='modal'>
@@ -40,27 +40,27 @@ const FormAlbum = ({formLabel, visibility, changeVisible, actionAlbum, isEdit, a
                 <div className='modal_body'>
                     <div className='form-group'>
                         <label htmlFor="albumName">Nom de l'album</label>
-                        <input type="text" name='albumName' id='albumName' placeholder='Album name' onChange={(e) => setAlbumName(e.currentTarget.value)} value={albumName} />
+                        <input type="text" name='albumName' id='albumName' placeholder='Album name' onChange={(e) => setAlbumName(e.currentTarget.value)} defaultValue={album?.name} />
                     </div>  
                     <div className='form-group'>
                         <label htmlFor="groupe">Groupe</label>
-                        <input type="text" name='groupe' id='groupe' placeholder='groupe name' onChange={(e) => setGroupe(e.currentTarget.value)} value={groupe}/>
+                        <input type="text" name='groupe' id='groupe' placeholder='groupe name' onChange={(e) => setGroupe(e.currentTarget.value)} defaultValue={album?.groupe}/>
                     </div>                                                        
                     <div className='form-group'>
                         <label htmlFor="style">Style de musique</label>
-                        <input type="text" name='styleOne' id='styleOne' placeholder='style' onChange={(e) => {setStyleOne(e.currentTarget.value)}} value={styleOne}/>
+                        <input type="text" name='styleOne' id='styleOne' placeholder='style' onChange={(e) => {setStyleOne(e.currentTarget.value)}} defaultValue={album?.styleOne}/>
                     </div>
                     <div className='form-group'>
                         <label htmlFor="nbtitle">Nombre de titres</label>
-                        <input type="number" name='nbtitle' id='nbTitle' min={0} onChange={(e)=>setNbTitle(parseInt(e.currentTarget.value))} value={nbTitle}/>
+                        <input type="number" name='nbtitle' id='nbTitle' min={0} onChange={(e)=>setNbTitle(parseInt(e.currentTarget.value))} defaultValue={album?.nb_title}/>
                     </div>                            
                     <div className='form-group'>
                         <label htmlFor="release-date">Date de sortie</label>
-                        <input type="date" name='releaseDate' id='release-date' onChange={(e) => setReleaseDate(e.currentTarget.value)} value={releaseDate}/>
+                        <input type="date" name='releaseDate' id='release-date' onChange={(e) => setReleaseDate(e.currentTarget.value)} defaultValue={album?.release_date}/>
                     </div>
                     <div className='form-group'>
                         <label htmlFor="release-date">URL Cover</label>
-                        <input type="text" name='image' id='image' onChange={(e) => setImage(e.currentTarget.value)} value={image}/>
+                        <input type="text" name='image' id='image' onChange={(e) => setImage(e.currentTarget.value)} defaultValue={album?.image}/>
                     </div>
                     {/* {!isEdit && */}
                         <button type="submit" onClick={
