e7bbf2f470b3822a2d0208d51b25ab2170918ad0
Jérémy Noble <jayhem10@MBP-de-Jeremy.lan>
2023-09-24 20:16:40 +0200

    update adding album logic

--- components/Album/AlbumAddDeezer.tsx
+++ components/Album/AlbumAddDeezer.tsx
@@ -1,9 +1,8 @@
-// pages/index.tsx
-
 import DeezerAlbum from "@/interfaces/deezer/deezerAlbum";
 import supabase from "@/utils/supabase";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import Loader from "../Ui/Loader";
 import AlbumList from "./AlbumListDeezer";
 import Search from "./AlbumSearchDeezer";
 
@@ -14,32 +13,76 @@ type Props = {
 };
 
 const AlbumAddDeezer = ({ visibility, changeVisible, getAlbums }: Props) => {
-  const [albums, setAlbums] = useState<DeezerAlbum[] | null | undefined>();
-  const [selectedAlbum, setSelectedAlbum] = useState<DeezerAlbum | null>(null);
-  const [addingAlbum, setAddingAlbum] = useState<DeezerAlbum | null>(null);
+  const [albums, setAlbums] = useState<DeezerAlbum[] | null | undefined>(); 
+  const [searchingAlbums, setSearchingAlbums] = useState<boolean>(false); 
+  const [selectedAlbum, setSelectedAlbum] = useState<DeezerAlbum | null>(null); // album sélectionné api deezer
+  const [albumToAdd, setAlbumToAdd] = useState<DeezerAlbum | null>(null); // l'album qui sera ajouter en base
+  let [lastInsertedId, setLastInsertedId] = useState<number>(0); // Initialize with null
 
-  let [lastInsertedId, setLastInsertedId] = useState<number>(0);
-  
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [albumAlreadyExist, setAlbumAlreadyExist] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (albumToAdd) {
+      getAlbumByDeezerId();
+    }
+  }, [albumToAdd]);
+
+  useEffect(() => {
+    if (isChecked && !albumAlreadyExist && albumToAdd) {
+      sendData(albumToAdd);
+    }
+  }, [isChecked, albumAlreadyExist]);
+
+  useEffect(() => {
+    fetchLastAlbum();
+  }, []);
+
+
   const searchAlbums = async (query: string) => {
-    setSelectedAlbum(null)
-    setAddingAlbum(null)
+    setSelectedAlbum(null);
+    setAlbumToAdd(null);
+    setSearchingAlbums(true);
     try {
       const response = await fetch(`/api/search?album=${query}`);
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-      const jsonData = await response.json();
-      setAlbums(jsonData as DeezerAlbum[]);
+      if (response.ok) {
+        const jsonData = await response.json();
+        setAlbums(jsonData as DeezerAlbum[]);
+        setSearchingAlbums(false);
+      }
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
+  async function fillNewAlbum() {
+    try {
+      const response = await fetch(
+        `/api/getalbum?album_id=${selectedAlbum?.id}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      if (response.ok) {
+        const jsonData = await response.json();
+        setAlbumToAdd(jsonData as DeezerAlbum);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+
+    await fetchLastAlbum();
+  }
+
   const emptySearch = () => {
     setAlbums(null);
     setSelectedAlbum(null);
-    setAddingAlbum(null)
-
+    setAlbumToAdd(null);
+    setAlbumAlreadyExist(false)
+    setIsChecked(false)
   };
 
   const handleSelectAlbum = (album: DeezerAlbum | null) => {
@@ -47,53 +90,33 @@ const AlbumAddDeezer = ({ visibility, changeVisible, getAlbums }: Props) => {
     setAlbums([]);
   };
 
-  async function fillNewAlbum() {
-    try {
-      const response = await fetch(`/api/getalbum?album_id=${selectedAlbum?.id}`);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      if (response.ok) {
-        const jsonData = await response.json();
-        setAddingAlbum(jsonData as DeezerAlbum);
-      }
-
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-
-    await fetchLastAlbum();
-    const existAlbum = await getAlbumByDeezerId();
-    if (existAlbum == null && addingAlbum) {
-      await sendData(addingAlbum);  
-    }else{
-      toast.info("This album already exists.")
-    }
-  }
-
-  async function sendData(addingAlbum : DeezerAlbum) {
+  async function sendData(albumToAdd: DeezerAlbum) {
     const data = {
       id: ++lastInsertedId,
-      name: addingAlbum?.title,
-      groupe: addingAlbum?.artist.name,
-      nb_title: addingAlbum?.nb_tracks,
-      styleOne: addingAlbum?.genres?.data[0].name,
-      release_date: addingAlbum?.release_date,
-      image: addingAlbum?.cover_big,
-      deezer_id: addingAlbum?.id,
+      name: albumToAdd?.title,
+      groupe: albumToAdd?.artist.name,
+      nb_title: albumToAdd?.nb_tracks,
+      styleOne: albumToAdd?.genres?.data[0].name,
+      release_date: albumToAdd?.release_date,
+      image: albumToAdd?.cover_big,
+      deezer_id: albumToAdd?.id,
     };
     try {
       let { error } = await supabase.from("albums").insert([data]);
       if (error) throw error;
-      toast.success("Album added with success !");
+      showToast("success", "Album added with success !");
       setSelectedAlbum(null);
-      setAlbums(null)
+      setAlbums(null);
       changeVisible(false);
+      setIsChecked(false);
+      setAlbumAlreadyExist(false);
       getAlbums();
     } catch (errorAdd) {
-      toast.error("An error has occured, you album have not been added !");
+      showToast(
+        "error",
+        "An error has occurred, your album has not been added !"
+      );
     }
-
   }
 
   async function fetchLastAlbum() {
@@ -102,13 +125,11 @@ const AlbumAddDeezer = ({ visibility, changeVisible, getAlbums }: Props) => {
       .select("id")
       .order("id", { ascending: false });
     if (data) {
-      setLastInsertedId(data[0].id);
+      setLastInsertedId(data[0]?.id || 0); // Set to 0 if no data
     }
   }
 
-  useEffect(() => {
-    fetchLastAlbum();
-  });
+
 
   const getAlbumByDeezerId = async () => {
     const { data, error } = await supabase
@@ -116,28 +137,54 @@ const AlbumAddDeezer = ({ visibility, changeVisible, getAlbums }: Props) => {
       .select()
       .eq("deezer_id", selectedAlbum?.id);
 
-      if (data && data?.length > 0) {
-        return data;
-      }else{
-        return null;
-      }
+    if (data && data?.length > 0) {
+      setIsChecked(true);
+      setAlbumAlreadyExist(true);
+      showToast("info", "This album already exist");
+    } else {
+      setIsChecked(true);
+      setAlbumAlreadyExist(false);
+    }
+  };
+
+  const showToast = (type: string, message: string) => {
+    switch (type) {
+      case "success":
+        toast.success(message);
+        break;
+      case "info":
+        toast.info(message);
+        break;
+      case "error":
+        toast.error(message);
+        break;
+      default:
+        toast.info(message);
+        break;
+    }
   };
 
   return (
     <>
       {visibility && (
         <div className="modal mt-20">
-          <div className="bg-[#030303] border-2 border-indigo-500/50 shadow-md rounded px-8 pt-6 pb-8 mb-4 w-[80vw] md:w-[50vw] max-h-[90vh] overflow-y-auto ">
+          <div className="bg-[#030303] border-2 border-indigo-500/50 shadow-md rounded px-2 md:px-8 pt-6 w-[80vw] md:w-[50vw] max-h-[90vh] overflow-y-auto ">
             <h1 className="my-3">Search for an album</h1>
             <Search onSearch={searchAlbums} emptySearch={emptySearch} />
-            {!selectedAlbum && (
+            {!selectedAlbum && !searchingAlbums &&
               <AlbumList albums={albums} onSelectAlbum={handleSelectAlbum} />
-            )}
+            }
+            {searchingAlbums && 
+              <div className="my-3">
+              <Loader/>
+              </div>
+            }
+
             {selectedAlbum && (
               <>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5 mb-3">
                   <div className="mx-auto">
-                    <img src={selectedAlbum.cover_medium} alt="" />
+                    <img src={selectedAlbum.cover_medium} alt="cover" />
                   </div>
                   <div className="text-start my-auto">
                     <p className="font-bold text-xl">{selectedAlbum.title}</p>
@@ -160,7 +207,7 @@ const AlbumAddDeezer = ({ visibility, changeVisible, getAlbums }: Props) => {
               <a href="#" onClick={() => changeVisible(visibility)}>
                 <button
                   onClick={() => handleSelectAlbum(null)}
-                  className="m-2 hover:bg-[#4547a8] text-blue-50 dark:text-blue-100 font-semibold hover:text-white py-2 px-4 border border-[#4547a8] hover:border-transparent rounded"
+                  className="m-4 hover:bg-[#4547a8] text-blue-50 dark:text-blue-100 font-semibold hover:text-white py-2 px-4 border border-[#4547a8] hover:border-transparent rounded"
                 >
                   Close
                 </button>
--- components/Album/AlbumSearchDeezer.tsx
+++ components/Album/AlbumSearchDeezer.tsx
@@ -9,33 +9,31 @@ type Props = {
 const Search = ({ onSearch, emptySearch }: Props) => {
   const [query, setQuery] = useState("");
 
-  const handleSearch = () => {
+  const handleSearch = (e: { preventDefault: () => void; }) => {
+    e.preventDefault()
     onSearch(query);
   };
 
   return (
-    <div className="grid md:grid-cols-2 gap-2 grid-cols-1 ">
-      <div>
-        <input
-          type="text"
-          placeholder="Search by artist or album name"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          className="shadow appearance-none border rounded  w-full py-2 px-3 bg-[#313378] text-white focus:ring-indigo-500 focus:border-indigo-500 leading-tight focus:outline-none focus:shadow-outline my-3 mr-3"
-          autoFocus={true}
-        />
-      </div>
-      <div className="my-auto">
-        <button className="text-xs mr-2" onClick={() => handleSearch()}>
-          {/* <AiOutlineSearch size={20} /> */}
-          Search
-        </button>
-        <button className="text-xs" onClick={() => emptySearch()}>
-          {/* <AiOutlineClose size={20} /> */}
-          x
-        </button>
-      </div>
+    <form onSubmit={handleSearch}>
+    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+    <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm bg-[#313378] text-white focus:ring-indigo-500 focus:border-indigo-500 rounded-lg focus:outline-none focus:shadow-outline" placeholder="Search artists, albums..." required value={query} onChange={(e) => setQuery(e.target.value)} autoFocus={true}/>
+        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-800 invisible md:visible" >Search</button>
+        <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white rounded-r-lg focus:ring-4 focus:outline-none focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 visible md:invisible ">
+                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+                <span className="sr-only">Search</span>
+            </button>
     </div>
+</form>
+
   );
 };
 
